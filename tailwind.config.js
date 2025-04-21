84dc5a10f0f6db0f47b749f84d27cdc87f279dc1
Mathews <baiomathews@gmail.com>
2025-04-21 15:24:18 -0300

    Correção no Tailwind e menu

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.razor',
+        './**/*.html',
+        './**/*.cshtml',
+        './wwwroot/**/*.html'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#151773',
+                secondary: '#4f52d9',
+                highlight: "#76be11"
+            }
+        },
+    },
+    plugins: [],
+}
