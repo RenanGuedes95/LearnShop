a27dedae947f354396e873e1193dc1d3b72929a4
William Silva <william.silva@viannasempre.com.br>
2025-04-22 16:43:43 -0300

    Adição de melhorias na apresenetação do componente ProducstCard - Adição das propriedades Publisher e Category na model Ebook.

--- Models/Ebook.cs
+++ Models/Ebook.cs
@@ -7,6 +7,8 @@ namespace LearnShop.Models
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
         public decimal Price { get; set; }
     }
 }
